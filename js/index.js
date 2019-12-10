88fcd8f8c8f47784aa09623b2988924b93606e02
slv-improver <k.moret.slv@gmail.com>
2019-12-10 09:02:13 +0100

    1ere version du slide, eventlistener, marker

--- js/index.js
+++ js/index.js
@@ -1,4 +1,65 @@
-// ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=7b223c89d17e9f045f732705dd821673730331be", function(response) {
-//     console.log(response);
-// })
+function reloadSlide () {
+    slideImg.src = 'images/' + urlList[slideCount];
+    slideCaption.textContent = captionList[slideCount];
+}
 
+function slideNext () {
+    if (slideCount < urlList.length-1) {
+        slideCount++;
+    } else {
+        slideCount = 0;
+    }
+    reloadSlide();
+}
+function slidePrevious () {
+    if (slideCount > 0) {
+        slideCount--;
+    } else {
+        slideCount = urlList.length-1;
+    }
+    reloadSlide();
+}
+
+let urlList = ['people1.jpg', 'station3.jpg'];
+let captionList = ['Bienvenue à : EnRoute, EnLigne', 'Venez prendre votre vélo à la station']
+
+let slideImg = document.getElementById('slide').childNodes[1].childNodes[1];
+let slideCaption = document.getElementById('slide').childNodes[1].childNodes[3];
+
+let slideCount = 0;
+reloadSlide();
+
+let intervalId = setInterval(slideNext, 5000);
+
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode === 39) {
+        slideNext();
+    } else if  (e.keyCode === 37) {
+        slidePrevious();
+    }
+})
+
+let btnPrevious = document.getElementById('nav-icons').childNodes[1];
+let btnControl = document.getElementById('nav-icons').childNodes[3];
+let btnNext = document.getElementById('nav-icons').childNodes[5];
+
+btnPrevious.addEventListener('click', slidePrevious);
+btnNext.addEventListener('click', slideNext);
+btnControl.addEventListener('click', function(e) {
+    if (e.target.textContent === 'pause') {
+        clearInterval(intervalId);
+        e.target.textContent = 'play_arrow';
+    } else if (e.target.textContent === 'play_arrow') {
+        intervalId = setInterval(slideNext, 5000);
+        e.target.textContent = 'pause';
+    }
+})
+
+/* API */
+ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Brisbane&apiKey=7b223c89d17e9f045f732705dd821673730331be", function(response) {
+    let brisbane = JSON.parse(response);
+    console.log(brisbane);
+    brisbane.forEach(element => {
+        L.marker([element.position.lat, element.position.lng]).addTo(mymap);
+    });
+});
--- js/map.js
+++ js/map.js
@@ -1,12 +1,10 @@
-var map = new ol.Map({
-target: 'map',
-layers: [
-    new ol.layer.Tile({
-    source: new ol.source.OSM()
-    })
-],
-view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
-    zoom: 4
-})
-});
+var mymap = L.map('mapid').setView([-27.470125, 153.021072], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2x2LWltcHJvdmVyIiwiYSI6ImNrM3hjZWZoeTBwOTMzZXA2NWZoYWcyN2MifQ.abeHwAe3XTh0mgdyror_Uw', {
+  maxZoom: 20,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/streets-v11'
+}).addTo(mymap);
+
