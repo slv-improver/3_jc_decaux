33a32d66bdb75a8842e1d78f4c6aa0be9ece77b5
slv-improver <k.moret.slv@gmail.com>
2019-12-13 23:51:03 +0100

    eventlister on marker

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+function slidePlay() {
+    slideNext();
+}
 function reloadSlide () {
     slideImg.src = 'images/' + urlList[slideCount];
     slideCaption.textContent = captionList[slideCount];
@@ -19,6 +22,15 @@ function slidePrevious () {
     }
     reloadSlide();
 }
+function control(e) {
+    if (e.target.textContent === 'pause') {
+        clearInterval(intervalId);
+        e.target.textContent = 'play_arrow';
+    } else if (e.target.textContent === 'play_arrow') {
+        intervalId = setInterval(slidePlay, 5000);
+        e.target.textContent = 'pause';
+    }
+}
 
 let urlList = ['people1.jpg', 'station3.jpg'];
 let captionList = ['Bienvenue à : EnRoute, EnLigne', 'Venez prendre votre vélo à la station']
@@ -29,7 +41,7 @@ let slideCaption = document.getElementById('slide').childNodes[1].childNodes[3];
 let slideCount = 0;
 reloadSlide();
 
-let intervalId = setInterval(slideNext, 5000);
+let intervalId = setInterval(slidePlay, 5000);
 
 document.addEventListener('keyup', function(e) {
     if (e.keyCode === 39) {
@@ -42,24 +54,29 @@ document.addEventListener('keyup', function(e) {
 let btnPrevious = document.getElementById('nav-icons').childNodes[1];
 let btnControl = document.getElementById('nav-icons').childNodes[3];
 let btnNext = document.getElementById('nav-icons').childNodes[5];
-
 btnPrevious.addEventListener('click', slidePrevious);
 btnNext.addEventListener('click', slideNext);
-btnControl.addEventListener('click', function(e) {
-    if (e.target.textContent === 'pause') {
-        clearInterval(intervalId);
-        e.target.textContent = 'play_arrow';
-    } else if (e.target.textContent === 'play_arrow') {
-        intervalId = setInterval(slideNext, 5000);
-        e.target.textContent = 'pause';
-    }
-})
+btnControl.addEventListener('click', control);
 
 /* API */
 ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Brisbane&apiKey=7b223c89d17e9f045f732705dd821673730331be", function(response) {
     let brisbane = JSON.parse(response);
     console.log(brisbane);
     brisbane.forEach(element => {
-        L.marker([element.position.lat, element.position.lng]).addTo(mymap);
+        let station = L.marker([element.position.lat, element.position.lng], {
+            icon: bikeIcon}).addTo(mymap).bindPopup(element.address);
+        station.addEventListener("click", function () {
+            document.getElementById('station').style.display = 'block';
+            document.getElementById('address').textContent = element.address;
+            document.getElementById('places').textContent = element.bike_stands;
+            document.getElementById('available').textContent = element.available_bike_stands;
+        })
     });
+});
+
+/* hidden station details */
+document.getElementById('mapid').addEventListener("click", function (e) {
+    if (document.getElementById('mapid').style.outline === 'none') {
+        document.getElementById('station').style.display = 'none';
+    }
 });
--- js/map.js
+++ js/map.js
@@ -1,5 +1,13 @@
 var mymap = L.map('mapid').setView([-27.470125, 153.021072], 13);
 
+var bikeIcon = L.icon({
+  iconUrl: 'images/bikemap.png',
+
+  iconSize:     [41.3, 48.5],
+  iconAnchor:   [20.65, 24.25],
+  popupAnchor:  [0, -24.25]
+});
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2x2LWltcHJvdmVyIiwiYSI6ImNrM3hjZWZoeTBwOTMzZXA2NWZoYWcyN2MifQ.abeHwAe3XTh0mgdyror_Uw', {
   maxZoom: 20,
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
